d9e7f45819cb6b787aa2333c9540281d8a07915b
abhinandpn <abhinandpn4067@gmail.com>
2026-03-03 00:59:15 +0530

    updating

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abhinandpn/StockX
+
+go 1.25.0
--- pkg/domain/admin.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type Admin struct {
-	Id       uint   `json:"id" gorm:"primary key"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
